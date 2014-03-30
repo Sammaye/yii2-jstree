ccfd3bc98047adaddff3c57066bfd0ec8272c52a
Sammaye <sam.millman@googlemail.com>
2014-03-30 11:29:05 +0100

    Fixed autoloading of asset

--- JsTree.php
+++ JsTree.php
@@ -6,7 +6,7 @@ use Yii;
 use yii\helpers\Json;
 use yii\helpers\Html;
 use yii\widgets\InputWidget;
-use common\components\jstree\JsTreeAsset;
+use sammaye\jstree\JsTreeAsset;
 
 class JsTree extends InputWidget
 {
